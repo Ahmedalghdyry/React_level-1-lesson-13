b491d079b0e69a2a39323ba851dbb96318284173
Ahmedalghdyry <ahmdalghdyry31@gmail.com>
2024-11-06 20:35:38 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+import HTML from "./pages/html";
+import Css from "./pages/css";
+import Javascript from "./pages/javascript";
+// LEVEL2
+import {useContext } from "react";
+import ThemeContext from "./context/Datacontext";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1>SORROY.........</h1>,
+  },
+
+  {
+    path: "/html",
+    element: <HTML />,
+  },
+
+  {
+    path: "/css",
+    element: <Css />,
+  },
+  {
+    path: "/javascript",
+    element: <Javascript />,
+  },
+]);
+
+function App() {
+  const {theme} = useContext(ThemeContext);
+
+  return (
+    <div  className={`App ${theme}`}>
+    <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
